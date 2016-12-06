2afec405f3b2d8057c0017daff8130bd0726b579
Andreas Hagesjö <andreas.hagesjo@gmail.com>
2016-12-06 07:04:05 +0100

    Made bogusclient a bit more automatic

--- client.py
+++ client.py
@@ -3,27 +3,71 @@
 import asyncio
 import websockets
 import sys
+from time import sleep
 
 @asyncio.coroutine
 def hello():
-    websocket = yield from websockets.connect('ws://localhost:1234/training')
 
     while True:
+            websocket = None
         try:
-            name = input("> ")
-            if name == "quit" or name == "exit":
-                break
-            elif name == "reg":
-                yield from websocket.send('{"type" : "register", "name" : "legget"} ')
-            elif name == "start":
-                yield from websocket.send('{"type" : "start_game", "name" : "legget"}')
-            else:
-                yield from websocket.send(name)
-
+            websocket = yield from websockets.connect('ws://localhost:1234/training')
+            yield from websocket.send('{"type" : "register", "name" : "legget"} ')
             greeting = yield from websocket.recv()
             print("< {}".format(greeting))
+            sleep(1)
+
+            yield from websocket.send('{"type" : "start_game", "name" : "legget"}')
+            greeting = yield from websocket.recv()
+            print("< {}".format(greeting))
+            sleep(1)
+
+            for i in range(2):
+                yield from websocket.send('{"type" : "move", "direction" : "RIGHT", "plant_bomb" : false }')
+                greeting = yield from websocket.recv()
+                print("< {}".format(greeting))
+                sleep(1)
+
+            yield from websocket.send('{"type" : "move", "direction" : "DOWN", "plant_bomb" : false }')
+            greeting = yield from websocket.recv()
+            print("< {}".format(greeting))
+            sleep(1)
+
+
+            yield from websocket.send('{"type" : "move", "direction" : "DOWN", "plant_bomb" : true }')
+            greeting = yield from websocket.recv()
+            print("< {}".format(greeting))
+            sleep(1)
+
+            for i in range(7):
+                yield from websocket.send('{"type" : "move", "direction" : "DOWN", "plant_bomb" : false }')
+                greeting = yield from websocket.recv()
+                print("< {}".format(greeting))
+                sleep(1)
+
+
+            yield from websocket.close()
+            sleep(5)
         except:
-            pass
+            if websocket:
+                yield from websocket.close()
+
+    # while True:
+        # try:
+            # name = input("> ")
+            # if name == "quit" or name == "exit":
+                # break
+            # elif name == "reg":
+                # yield from websocket.send('{"type" : "register", "name" : "legget"} ')
+            # elif name == "start":
+                # yield from websocket.send('{"type" : "start_game", "name" : "legget"}')
+            # else:
+                # yield from websocket.send(name)
+
+            # greeting = yield from websocket.recv()
+            # print("< {}".format(greeting))
+        # except:
+            # pass
 
 
 asyncio.get_event_loop().run_until_complete(hello())
