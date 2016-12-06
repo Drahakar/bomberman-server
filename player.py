572c49529a1ba58592d2acbf4ab6a12fa82192b6
Andreas Hagesjö <andreas.hagesjo@gmail.com>
2016-12-06 03:15:29 +0100

    Created setup function

--- player.py
+++ player.py
@@ -1,11 +1,16 @@
 from uuid import uuid4
+from bomb import Bomb
 
 class Player:
     def __init__(self, name):
         self.name = name
         self.p_id = str(uuid4())
-        self.coordinate = None
-        self.alive = False
+        self.setup()
+
+    def setup(self):
+        self.coord = None
+        self.alive = True
+        self.num_bombs = 1
 
     def __str__(self):
         return "Name: {}, id: {}".format(self.name, self.p_id)
