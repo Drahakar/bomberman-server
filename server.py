a5c66ff29b86a0774a279f25e0b6751b7f400eba
Andreas Hagesjö <andreas.hagesjo@gmail.com>
2016-12-12 09:53:19 +0100

    Restructed server to fit multiple clients in same game.
    Previously, clients could only start a game with themselves. This is now
    fixed.
    Although, bug introduced: Won't register when a client disconnects.

--- server.py
+++ server.py
@@ -24,70 +24,116 @@ class Server:
             "move" : self.register_move,
         }
 
-
     def start(self):
         start_server = websockets.serve(self.request_handler, self.host, self.port)
-
         asyncio.get_event_loop().run_until_complete(start_server)
         asyncio.get_event_loop().run_forever()
 
     @asyncio.coroutine
     def request_handler(self, websocket, path):
-        self.clients.add(websocket)
+        client = ClientConnect(websocket)
+        self.clients.add(client)
+        logging.info("Connected clients: {}".format(len(self.clients)))
         while True:
             try:
-                start = time()
-                msg = yield from websocket.recv()
-                response_time = time() - start
-                logging.info("Client took {} to respond".format(time() - start))
-                logging.info("Message recieved: {}".format(msg))
                 # if self.time_handler(response_time, websocket):
                     # yield from websocket.send("WARNING: you're too slow")
                 # else:
                     # yield from self.message_handler(msg, websocket)
-                logging.debug("Client sent message: {}".format(msg))
-                yield from self.message_handler(msg, websocket)
-                logging.info("Connected clients: {}".format(len(self.clients)))
-                logging.info("Registered players: {}".format(self.registered_players))
+
+                listener_task = asyncio.ensure_future(client.get_message())
+                sender_task = asyncio.ensure_future(client.prepare_send())
+                done, pending = yield from asyncio.wait(
+                    [listener_task, sender_task],
+                    return_when=asyncio.FIRST_COMPLETED)
+
+                if listener_task in done:
+                    yield from client.parse_incoming(self)
+                else:
+                    listener_task.cancel()
+
+                if sender_task in done:
+                    msg_to_send = sender_task.result()
+                    if msg_to_send:
+                        yield from client.send_message(msg_to_send)
+                else:
+                    sender_task.cancel()
+
             except ConnectionClosed as e:
-                self.clients.remove(websocket)
+                self.clients.remove(client)
                 if websocket in self.registered_players:
-                    del(self.registered_players[websocket])
+                    del(self.registered_players[client])
                 logging.info("Removed connection. Current connections: {}".format(len(self.clients)))
                 break
 
     def time_handler(self, response_time, websocket):
         return response_time > 3 and websocket in self.player_game
 
-    def message_handler(self, message, websocket):
+    def message_handler(self, message, client):
         try: 
             message = json.loads(message)
-            return self.types[message["type"]](message, websocket)
+            return self.types[message["type"]](message, client)
         except (ValueError, KeyError) as e:
             logging.debug(traceback.print_exc())
-            return websocket.send("Invalid request: {}".format(e))
+            return "Invalid request: {}".format(e)
 
-    def register_player(self, message, websocket):
-        if websocket in self.registered_players:
-            return websocket.send("Player already registered")
+    def register_player(self, message, client):
+        if client in self.registered_players:
+            return "Player already registered"
         else:
-            self.registered_players[websocket] = Player(message["name"])
-            return websocket.send("Player registered")
+            self.registered_players[client] = Player(message["name"])
+            # if len(self.registered_players) == 1:
+                # self.start_game()
+            logging.info("Registered players: {}".format(self.registered_players))
+            return "Player registered"
 
-    def start_game(self, message, websocket):
-        if websocket in self.player_game:
-            return websocket.send("Can't play two games at once")
-        game = Game([self.registered_players[websocket]], 0)
-        self.player_game[websocket] = game
-        self.games.add(game)
-        game.start()
-        return websocket.send("{}\n{}".format(game.world_map.to_json(), game.world_map.to_ascii()))
-
-    def register_move(self, message, websocket):
-        if websocket not in self.player_game:
-            return websocket.send("You are not in a game")
-        current_player = self.registered_players[websocket]
-        current_game = self.player_game[websocket]
-        current_game.get_player_move(current_player, (message["direction"], message["plant_bomb"]))
+    def register_move(self, message, client):
+        if client not in self.player_game:
+            return "You are not in a game"
+        current_player = self.registered_players[client]
+        current_game = self.player_game[client]
+        current_game.get_player_move(client, (message["direction"], message["plant_bomb"]))
         current_game.tick_handler()
-        return websocket.send("{}\n{}".format(current_game.world_map.to_json(), current_game.world_map.to_ascii()))
+
+    def start_game(self, a=1, b=1):
+        game = Game(self.registered_players, 0)
+        logging.info("Starting game")
+        for player in self.registered_players:
+            self.player_game[player] = game
+        for client in self.registered_players:
+            client.manual_output("{}\n{}".format(game.world_map.to_json(), game.world_map.to_ascii()))
+        return "Sent Gamestart to people"
+
+
+class ClientConnect:
+
+    def __init__(self, websocket):
+        self.ws = websocket
+        self.incoming = asyncio.Queue()
+        self.outgoing = asyncio.Queue()
+
+    def manual_output(self, message):
+        self.outgoing.put_nowait(message)
+
+    @asyncio.coroutine
+    def get_message(self):
+        msg_in = yield from self.ws.recv()
+        logging.info("Client sent message: {}".format(msg_in))
+        yield from self.incoming.put(msg_in)
+
+    @asyncio.coroutine
+    def parse_incoming(self, server):
+        msg_to_consume = yield from self.incoming.get()
+        msg_to_consume = server.message_handler(msg_to_consume, self)
+        if msg_to_consume:
+            yield from self.outgoing.put(msg_to_consume)
+
+    @asyncio.coroutine
+    def send_message(self, message):
+        logging.info("Sending message: {}".format(message))
+        yield from self.ws.send(message)
+
+    @asyncio.coroutine
+    def prepare_send(self):
+        msg_out = yield from self.outgoing.get()
+        return msg_out
