39f8cc8ec2383a61da3718f16c1b2e28e5b4eaa4
Andreas Hagesjö <andreas.hagesjo@gmail.com>
2016-12-22 17:04:22 +0100

    Removed hardcoded arguments and unused code.
    Moved host and port to the argparse.
    Removed initial test code.

--- main.py
+++ main.py
@@ -4,26 +4,14 @@ import argparse
 import logging
 from server import Server
 
-@asyncio.coroutine
-def hello(websocket, path):
-    name = yield from websocket.recv()
-    print("NAME RECIEVEVED")
-    print("< {}".format(name))
-    greeting = "LEGGET"
-    yield from websocket.send(greeting)
-    print("> {}".format(greeting))
-
 def main():
-    # import pdb; pdb.set_trace()
-    # logger = logging.getLogger('websockets')
-    # logger.setLevel(logging.INFO)
-    # logger.addHandler(logging.StreamHandler())
-
-    parser = argparse.ArgumentParser(description="Snakebot for cygni")
+    parser = argparse.ArgumentParser(description="Bomberman websocket server")
+    parser.add_argument("listen_host", help="Hosts to listen to.")
+    parser.add_argument("listen_port", help="Port to listen to.", type=int)
     parser.add_argument("-l", "--log", dest="loglevel",  help="Loglevel. DEBUG|INFO|WARNING|ERROR|CRITICAL", default="INFO")
     args = parser.parse_args()
 
-    bomb_server = Server(args.loglevel, 'localhost', 1234)
+    bomb_server = Server(args.loglevel, args.listen_host, args.listen_port)
     bomb_server.start()
 
 
